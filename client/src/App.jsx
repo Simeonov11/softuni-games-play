d031687800fbbe450be8bf19614638f19e19677b
Simo <simeonov.pa@gmail.com>
2025-03-10 17:25:21 +0000

    Extract components header, home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import './App.css';
+import Header from './components/header/header.jsx';
+import Home from './components/home/Home.jsx';
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <div id="box">
+            <Header />
 
-            
-
+            <main id="main-content">
+                <Home />
+            </main>
         </div>
     )
 }
