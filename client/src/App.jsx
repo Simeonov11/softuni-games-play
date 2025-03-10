13ccde81663145d90f5b20b1ba65f6fdf459671f
Simo <simeonov.pa@gmail.com>
2025-03-10 21:21:05 +0000

    Show edit page with pre populated values in the form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
                     <Route path='/login' element={<Login />} />
                     <Route path='/register' element={<Register />} />
                     <Route path='/games/:gameId/details' element={<GameDetails />} />
-                    {/* <Route path='/games/edit' element={<GameEdit />} /> */}
+                    <Route path='/games/:gameId/edit' element={<GameEdit />} />
                 </Routes>
                 
             </main>
